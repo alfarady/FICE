8027afabd52b4dfd52b12214182344e6a02416a2
Alfarady Raja <radenalfa9@gmail.com>
2019-05-14 21:11:03 +0700

    First development

---
+++ app/src/main/java/id/web/runup/fice/mvp/done/DoneActivity.java
@@ -0,0 +1,15 @@
+package id.web.runup.fice.mvp.done;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.web.runup.fice.R;
+
+public class DoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done);
+    }
+}
